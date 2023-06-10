c3545629823e477c28fa78416856c4dfb7bb1a37
lightnonstop <umichaeledet003@gmail.com>
2023-06-10 20:50:50 +0100

    change component header

---
+++ src/pages/ColorList.tsx
@@ -0,0 +1,46 @@
+import { Table } from 'antd';
+interface data1PropsObj{
+	key: number;
+	name: string;
+	product: number;
+	status: string;
+}
+const columns = [
+	{
+		title: 'S/N',
+		dataIndex: 'key',
+	},
+	{
+		title: 'Name',
+		dataIndex: 'name',
+	},
+	{
+		title: 'Product',
+		dataIndex: 'product',
+	},
+	{
+		title: 'Status',
+		dataIndex: 'status',
+	},
+];
+const data1: data1PropsObj[] = [];
+for (let i = 0; i < 46; i++){
+	data1.push({
+		key: i,
+		name: `Edward king ${i}`,
+		product: 32,
+		status: `London, Park Lane no. ${i}`,
+	});
+}
+function ColorList(){
+	return (
+		<div>
+			<h3 className='mb-4'>Color List</h3>
+			<div>
+				<Table columns={columns} dataSource={data1} />
+			</div>
+		</div>
+	)
+}
+
+export default ColorList;
