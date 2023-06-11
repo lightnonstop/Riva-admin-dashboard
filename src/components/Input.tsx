6fafa180c304804c14c7006cccb031b03e4317e1
lightnonstop <umichaeledet003@gmail.com>
2023-06-12 00:21:06 +0100

    add name property for input form

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -3,8 +3,9 @@ type InputProps = {
     label: string;
     i_id: string;
     i_class?: string;
+    name?: string;
 };
-function Input({ type, label, i_id, i_class }: InputProps) {
+function Input({ type, label, i_id, i_class, name }: InputProps) {
   return (
     <div className='form-floating mb-3'>
         <input 
@@ -12,6 +13,7 @@ function Input({ type, label, i_id, i_class }: InputProps) {
         className={i_class ? `form-control ${i_class}` : 'form-control'}
         id={i_id}
         placeholder={label}
+        name={name}
         />
         <label htmlFor="floatingInput">{label}</label>
     </div>
