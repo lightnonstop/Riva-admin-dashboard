a367f002d27601d27ea447b4601580eb5d7d7d18
lightnonstop <umichaeledet003@gmail.com>
2023-06-12 12:53:30 +0100

    write and update redux store

---
+++ src/app/store.ts
@@ -0,0 +1,24 @@
+import { configureStore } from "@reduxjs/toolkit";
+import authReducer from '../features/auth/authSlice';
+import customerReducer from '../features/customers/customerSlice';
+import productReducer from '../features/products/productSlice';
+import brandReducer from '../features/brands/brandSlice';
+import categoryReducer from '../features/categories/categorySlice';
+import colorReducer from '../features/colors/colorSlice';
+import blogReducer from '../features/blogs/blogSlice';
+import blogCategoryReducer from '../features/blogCategories/blogCategorySlice';
+import { useDispatch } from "react-redux";
+export const store = configureStore({
+    reducer: {
+        auth: authReducer,
+        customers: customerReducer,
+        products: productReducer,
+        brands: brandReducer,
+        categories: categoryReducer,
+        colors: colorReducer,
+        blogs: blogReducer,
+        blogCategories: blogCategoryReducer,
+    },
+});
+export type AppDispatch = typeof store.dispatch
+export const useAppDispatch: () => AppDispatch = useDispatch;
