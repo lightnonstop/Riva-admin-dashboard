a4a7e7d79392783f13fb32defb323bc83e27a26e
lightnonstop <umichaeledet003@gmail.com>
2023-06-12 04:43:29 +0100

    add onChange prop and edit margin

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -5,10 +5,11 @@ type InputProps = {
     i_class?: string;
     name?: string;
     value?: string;
+    onChange?: any;
 };
-function Input({ type, label, i_id, i_class, name, value }: InputProps) {
+function Input({ type, label, i_id, i_class, name, value, onChange }: InputProps) {
   return (
-    <div className='form-floating mb-3'>
+    <div className='form-floating mt-3 mb-2'>
         <input 
         type={type} 
         className={i_class ? `form-control ${i_class}` : 'form-control'}
@@ -16,6 +17,7 @@ function Input({ type, label, i_id, i_class, name, value }: InputProps) {
         placeholder={label}
         name={name}
         value={value}
+        onChange={onChange}
         />
         <label htmlFor="floatingInput">{label}</label>
     </div>
