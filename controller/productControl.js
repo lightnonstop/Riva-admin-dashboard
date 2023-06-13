dfe3228062604f90a071d90f82bdc671c8f218ee
lightnonstop <umichaeledet003@gmail.com>
2023-06-13 05:52:10 +0100

    remove upload image controller and setup as separate module

--- controller/productControl.js
+++ controller/productControl.js
@@ -3,8 +3,6 @@ const asyncHandler = require("express-async-handler");
 const slugify = require("slugify");
 const User = require("../models/userModel");
 const validateMongodbId = require('../utils/validateMongodbId');
-const { cloudinaryUploadImage,cloudinaryDeleteImage } = require('../utils/cloudinary');
-const fs = require('fs');
 const createProduct = asyncHandler(async (req, res) => {
   if (req.body.title) {
     req.body.slug = slugify(req.body.title);
@@ -193,36 +191,6 @@ const rating = asyncHandler(async (req, res) => {
   }
 });
 
-const uploadImages = asyncHandler(async (req, res) => {
-  try {
-    const uploader = (path) => cloudinaryUploadImage(path, 'images');
-    const urls = [];
-    const files = req.files;
-    for (const file of files){
-      const { path } = file;
-      const newPath = await uploader(path);
-      urls.push(newPath);
-      fs.unlinkSync(path);
-    }
-    const images = urls.map(file => {
-      return file;
-    });
-    res.json(images);
-  } catch(e){
-    throw new Error(e);
-  }
-});
-
-const deleteImages = asyncHandler(async (req, res) => {
-  const { id } = req.params;
-  try {
-    const remover = cloudinaryDeleteImage(id, 'images');
-    res.json({meassage: 'deleted'});
-  } catch(e){
-    throw new Error(e);
-  }
-});
-
 module.exports = {
   createProduct,
   getProduct,
@@ -231,6 +199,4 @@ module.exports = {
   removeProduct,
   addToWishlist,
   rating,
-  uploadImages,
-  deleteImages,
 };
