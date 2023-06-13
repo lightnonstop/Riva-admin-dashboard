897d7cebd38ddc88466b0d1becff64cb0f1482da
lightnonstop <umichaeledet003@gmail.com>
2023-06-13 09:53:45 +0100

    delete image using id

--- src/features/uploads/uploadService.ts
+++ src/features/uploads/uploadService.ts
@@ -3,13 +3,18 @@ import { config } from "../auth/authService";
 import { baseUrl } from "../../utils/base_url";
 
 const uploadImages = async (data: any) => {
-    const response = await axios.post(`${baseUrl}`, data, config);
+    const response = await axios.post(`${baseUrl}upload`, data, config);
     return response.data;
 };
 
+const deleteImages = async (id: string) => {
+    const response = await axios.delete(`${baseUrl}upload/delete-img/${id}`, config);
+    return response.data;
+};
 
 const uploadService = {
     uploadImages,
+    deleteImages,
 };
 
 export default uploadService;
