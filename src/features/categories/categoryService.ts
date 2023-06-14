fcd43cd771904270dbba89ade12294d2d9ebd33e
lightnonstop <umichaeledet003@gmail.com>
2023-06-14 09:19:48 +0100

    fix path name bug

--- src/features/categories/categoryService.ts
+++ src/features/categories/categoryService.ts
@@ -1,16 +1,21 @@
 import axios from "axios";
 import { baseUrl } from "../../utils/base_url";
-
+import { config } from "../auth/authService";
 
 const getCategories = async () => {
     const response = await axios.get(`${baseUrl}category-product/`);
     
     return response.data;
 };
-
+const createCategories = async (category: {}) => {
+    const response = await axios.post(`${baseUrl}category-product/`, category, config);
+    
+    return response.data;
+};
 
 const categoryService = {
     getCategories,
+    createCategories,
 };
 
 export default categoryService;
