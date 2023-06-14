fac8bbff98dcacf33752680f0e1384ad2629b6c6
lightnonstop <umichaeledet003@gmail.com>
2023-06-14 10:44:29 +0100

    fix unclear value and function naming

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { AddBlog, AddBlogCategory, AddColor, BlogCategoryList, BlogList, BrandList, CategoryList, ColorList, Customers, Dashboard, Enquiries, ForgotPassword, Login, Orders, ProductList, ResetPassword } from './pages';
+import { AddBlog, AddBlogCategory, AddColor, BlogCategories, Blogs, Brands, ProductCategories, Colors, Customers, Dashboard, Enquiries, ForgotPassword, Login, Orders, Products, ResetPassword } from './pages';
 import { MainLayout } from './components';
-import AddCategory from './pages/AddCategory';
 import AddBrand from './pages/AddBrand';
 import AddProduct from './pages/AddProduct';
+import AddProductCategory from './pages/AddProductCategory';
 function App() {
   return (
     <Router>
@@ -15,20 +15,20 @@ function App() {
         <Route path='/admin' element={<MainLayout />}>
           <Route index element={<Dashboard />} />
 					<Route path='enquiries' element={<Enquiries />} />
-					<Route path='blog-list' element={<BlogList />} />        
-					<Route path='blog-category-list' element={<BlogCategoryList />} />
-					<Route path='blog-category' element={<AddBlogCategory />} />
+					<Route path='blogs' element={<Blogs />} />        
+					<Route path='blog-categories' element={<BlogCategories />} />
+					<Route path='add-blog-category' element={<AddBlogCategory />} />
 					<Route path='orders' element={<Orders />} />
-					<Route path='color-list' element={<ColorList />} />
-					<Route path='color' element={<AddColor />} />
+					<Route path='colors' element={<Colors />} />
+					<Route path='add-color' element={<AddColor />} />
 					<Route path='customers' element={<Customers />} />
-					<Route path='category-list' element={<CategoryList />} />
-					<Route path='category' element={<AddCategory />} />
-					<Route path='brand-list' element={<BrandList />} />
-					<Route path='brand' element={<AddBrand />} />
-					<Route path='product-list' element={<ProductList />} />
-					<Route path='product' element={<AddProduct />} />
-					<Route path='blog' element={<AddBlog />} />
+					<Route path='product-categories' element={<ProductCategories />} />
+					<Route path='add-product-category' element={<AddProductCategory />} />
+					<Route path='brands' element={<Brands />} />
+					<Route path='add-brand' element={<AddBrand />} />
+					<Route path='products' element={<Products />} />
+					<Route path='add-product' element={<AddProduct />} />
+					<Route path='add-blog' element={<AddBlog />} />
 			</Route>
       </Routes>
     </Router>
