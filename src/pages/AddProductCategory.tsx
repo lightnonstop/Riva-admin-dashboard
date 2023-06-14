d627cd78e400985971dfa9f6a34c815b70cd4294
lightnonstop <umichaeledet003@gmail.com>
2023-06-15 00:28:43 +0100

    remove the navigate hook

--- src/pages/AddProductCategory.tsx
+++ src/pages/AddProductCategory.tsx
@@ -5,7 +5,7 @@ import { useSelector } from "react-redux";
 import { toast } from 'react-toastify';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
-import { useNavigate } from "react-router-dom";
+
 import { createAProductCategory, resetProductCategoryState } from '../features/productCategories/productCategorySlice';
 import { useEffect } from 'react';
 let schema = Yup.object().shape({
@@ -13,7 +13,7 @@ let schema = Yup.object().shape({
 })
 function AddProductCategory() {
   const dispatch = useDispatch<AppDispatch>();
-  const navigate = useNavigate();
+ 
 
   interface newCategoryProps{
 		isSuccess : string;
