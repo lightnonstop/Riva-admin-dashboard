d45df565e80aaca53e03d31d3fd1145d0a433c3c
lightnonstop <umichaeledet003@gmail.com>
2023-06-15 10:28:02 +0100

    ainclude title property in payload

--- src/features/productCategories/productCategorySlice.ts
+++ src/features/productCategories/productCategorySlice.ts
@@ -16,6 +16,31 @@ export const createAProductCategory = createAsyncThunk('category/create-product-
         return thunkAPI.rejectWithValue(e);
     }
 })
+
+export const getAProductCategory = createAsyncThunk('category/get-product-category', async (id: {}, thunkAPI) => {
+    try {
+        return await productCategoryService.getProductCategory(id);
+    } catch (e){
+        return thunkAPI.rejectWithValue(e);
+    }
+})
+
+export const updateAProductCategory = createAsyncThunk('category/update-product-category', async (updateData: { id: string; productCategoryValues: { title: string } }, thunkAPI) => {
+    try {
+        return await productCategoryService.updateProductCategory(updateData);
+    } catch (e){
+        return thunkAPI.rejectWithValue(e);
+    }
+})
+
+export const deleteAProductCategory = createAsyncThunk('category/delete-product-category', async (id: string, thunkAPI) => {
+    try {
+        return await productCategoryService.deleteProductCategory(id);
+    } catch (e){
+        return thunkAPI.rejectWithValue(e);
+    }
+})
+
 export const resetProductCategoryState = createAction('Reset_all');
 
 type initialStateProps = {
@@ -25,6 +50,9 @@ type initialStateProps = {
     isSuccess: boolean,
     message: string;
     createdProductCategory?: [];
+    productCategoryName?: string,
+    updatedProductCategory?: string,
+    deletedProductCategory?: [],
 }
 const initialState: initialStateProps = {
     productCategories: [],
@@ -75,6 +103,63 @@ export const productCategorySlice = createSlice({
                 state.isError = true,
                 state.isSuccess = false,
                 state.createdProductCategory = null!,
+                state.message = action.error.message!
+            })
+
+            .addCase(getAProductCategory.pending, (state) => {
+                state.isLoading = true
+            })
+
+            .addCase(getAProductCategory.fulfilled, (state, action) => {
+                state.isLoading = false,
+                state.isError = false,
+                state.isSuccess = true,
+                state.productCategoryName = action.payload.title!
+            })
+
+            .addCase(getAProductCategory.rejected, (state, action) => {
+                state.isLoading = false,
+                state.isError = true,
+                state.isSuccess = false,
+                state.productCategoryName = null!,
+                state.message = action.error.message!
+            })
+
+            .addCase(updateAProductCategory.pending, (state) => {
+                state.isLoading = true
+            })
+
+            .addCase(updateAProductCategory.fulfilled, (state, action) => {
+                state.isLoading = false,
+                state.isError = false,
+                state.isSuccess = true,
+                state.updatedProductCategory = action.payload!
+            })
+
+            .addCase(updateAProductCategory.rejected, (state, action) => {
+                state.isLoading = false,
+                state.isError = true,
+                state.isSuccess = false,
+                state.updatedProductCategory = null!,
+                state.message = action.error.message!
+            })
+
+            .addCase(deleteAProductCategory.pending, (state) => {
+                state.isLoading = true
+            })
+
+            .addCase(deleteAProductCategory.fulfilled, (state, action) => {
+                state.isLoading = false,
+                state.isError = false,
+                state.isSuccess = true,
+                state.deletedProductCategory = action.payload!
+            })
+
+            .addCase(deleteAProductCategory.rejected, (state, action) => {
+                state.isLoading = false,
+                state.isError = true,
+                state.isSuccess = false,
+                state.deletedProductCategory = null!,
                 state.message = action.error.message!
             })
             .addCase(resetProductCategoryState, () => initialState)
