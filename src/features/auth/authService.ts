7cb9f937357ca2320985edf801108ccbf1fbfa1e
lightnonstop <umichaeledet003@gmail.com>
2023-06-18 17:08:17 +0100

    remove token configuration for separate file

--- src/features/auth/authService.ts
+++ src/features/auth/authService.ts
@@ -1,13 +1,6 @@
 import axios from "axios";
 import { baseUrl } from "../../utils/base_url";
-const getTokenFromLocalStorage = localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user')!) : null;
 
-export const config = {
-    headers: {
-        Authorization: `Bearer ${getTokenFromLocalStorage.token}`,
-        Accept: 'application/json',
-    },
-}
 const login = async (userData: {}) => {
     const response = await axios.post(`${baseUrl}user/admin-login`, userData);
     if (response.data){
