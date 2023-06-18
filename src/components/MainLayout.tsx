6f533c927eed0fe204070cdfae847762c8c92d87
lightnonstop <umichaeledet003@gmail.com>
2023-06-18 17:07:43 +0100

    edit autoclose for toaster

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -214,7 +214,7 @@ function MainLayout() {
         >
           <ToastContainer
           position='top-right'
-          autoClose={2000}
+          autoClose={1000}
           hideProgressBar={false}
           newestOnTop={true}
           closeOnClick
